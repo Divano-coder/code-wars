feb4829040b079da6569afc7442eae838207435f
Divano-coder <divanodelarosa@gmail.com>
2023-08-19 18:21:57 -0600

    First kata

---
+++ reversed strings.js
@@ -0,0 +1,51 @@
+function solution(str) {
+  return str.split('').reverse().join('');
+}
+
+let strSol1 = "Word";
+let strSol2 = "World";
+
+console.log(solution(strSol1));
+console.log(solution(strSol2));
+
+
+
+// +++++++++++++++++
+
+// let answ = strSol1.split("");
+// console.log(answ);
+
+// ++++++++++++++++
+
+
+// let str1 = "Hola-soy-ivan";
+
+// let str2 = "Hola soy otra cadena";
+
+// let arraDeStr1 = str1.split();
+
+// console.log(arraDeStr1);
+
+// let arraDeStr2 = str2.split('');
+
+// console.log(arraDeStr2);
+
+// // +++++++++++++++++++++++++
+
+// let arr3 = [2, 3, 4, 5, 6];
+// let str4 = ["Hola", "soy", "ivan"];
+
+// let arr3Desordenado = arr3.reverse();
+// let str4Disordered = str4.reverse();
+
+// console.log(arr3Desordenado);
+// console.log(str4Disordered);
+// console.log(arr3);
+
+// // ++++++++++++++++++++++++
+
+// let elements = ["queso", "jamon", "pan"];
+// let strElements = elements.join(' ');
+
+// console.log(strElements);
+// console.log(elements);
