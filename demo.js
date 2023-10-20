76a07bfe6001adc95c2faa467e4f3ecde242f360
Divano-coder <divanodelarosa@gmail.com>
2023-10-19 20:28:02 -0600

    unnecesary files deleted

--- demo.js
+++
@@ -1,7 +0,0 @@
-let nameStr = "Ivan";
-let nameArray = nameStr.split();
-console.log(nameArray);
-
-let movies = ["interestellar", "movie-dick", "percy-jackson"];
-let moviesStr = movies.join();
-console.log(moviesStr);
